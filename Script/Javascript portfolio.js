018d8b4ee0972f5c1d4bb74755e1efd0c9f13298
Mats-Halkes <matsoh03@gmail.com>
2021-10-25 11:57:44 +0200

    Update Javascript portfolio.js

--- Script/Javascript portfolio.js
+++ Script/Javascript portfolio.js
@@ -1,4 +1,8 @@
-function siHei() {
-    console.log("Hei"); 
+var sum = 0;
+
+while (sum < 100) {
+  var tall = Number(prompt("Skriv et tall:"));
+  sum += tall;
 }
-    siHei();
+
+console.log(sum);

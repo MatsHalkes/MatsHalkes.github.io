f54b7d5d1c9ac6672707860f010cc7f9d449abf8
Mats-Halkes <matsoh03@gmail.com>
2021-10-27 09:22:10 +0200

    Update Javascript portfolio.js

--- Script/Javascript portfolio.js
+++ Script/Javascript portfolio.js
@@ -1,12 +1,18 @@
-var tall1 = Number(prompt("Skriv inn et tall:"));
-var tall2 = Number(prompt("Skriv inn et tall til:"));
+var gratis = 0<=3;
+var barn = 4<=15;
+var honør = 67;
 
-if (tall1>= tall2) {
-    console.log(tall1);
+var alder = Number(prompt("Hvor gammel er du?"));
+
+if (alder >= 0 && alder <= 3) {
+    console.log("Du er gratis");
 }
-else if (tall2 >= tall1){
-    console.log(tall2);
+else if (alder >= barn && alder <=15){
+    console.log("Du får 50% rabatt siden du er barn");
 }
-else if (tall1 == tall2) {
-    console.log("Tallene er like");
+else if (alder >= honør) {
+    console.log("Du får 50% rabatt siden du er honør");
+}
+else if (alder >= 15 && alder <= honør) {
+  console.log("Du får ikke noe rabatt");
 }

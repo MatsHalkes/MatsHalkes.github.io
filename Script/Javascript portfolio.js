5e6ea4e916dbc6e95d8759ce6ac87d3e9678d292
Mats-Halkes <matsoh03@gmail.com>
2021-11-01 12:59:00 +0100

    a

--- Script/Javascript portfolio.js
+++ Script/Javascript portfolio.js
@@ -1,18 +1,1 @@
-var gratis = 0;
-var barn = 4;
-var honør = 67;
 
-var alder = Number(prompt("Hvor gammel er du?"));
-
-if (alder >= 0 && alder <= 3) {
-    console.log("Du er gratis");
-}
-else if (alder >= barn && alder <=15){
-    console.log("Du får 50% rabatt siden du er barn");
-}
-else if (alder >= honør) {
-    console.log("Du får 50% rabatt siden du er honør");
-}
-else if (alder >= 16 && alder <= honør) {
-  console.log("Du får ikke noe rabatt");
-}
